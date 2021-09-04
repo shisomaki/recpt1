b22a6749d410de4a829ded7a8bdbb34eceb008f8
shiiya <shiiya@y.email.ne.jp>
2021-09-04 13:48:21 +0900

    add epg pids (when rescan PIDs)
    --sid オプションで、EPGを指定しても、Rescan ID が起きると、それ以降のEPGデータが出力されなくなるバグの修正
    
    参照：https://github.com/kaikoma-soft/recpt1/commit/1f4f88c3aecccf1cec93eaf0b6b37e346641ef90

--- recpt1/tssplitter_lite.c
+++ recpt1/tssplitter_lite.c
@@ -37,6 +37,8 @@ static char** AnalyzeSid(char *sid);
 static int AnalyzePmt(splitter *sp, unsigned char *buf, unsigned char mark);
 static int GetCrc32(unsigned char *data, int len);
 static int GetPid(unsigned char *data);
+static void addEpgPids( splitter *sp );
+static char *strcatNum( char *chosen_sid, int service_id);
 
 /**
  * サービスID解析
@@ -292,6 +294,7 @@ static int RescanPID(splitter *splitter, unsigned char *buf)
 			    splitter->pids[i] -= 1;
 		    }
 		}
+		addEpgPids( splitter );
 		fprintf(stderr, "Rescan PID End\n");
 	}
 
@@ -470,7 +473,8 @@ static int AnalyzePat(splitter *sp, unsigned char *buf)
 					sid_found = TRUE;
 					sp->pmt_version[sp->pmt_retain].pid = pid;
 					sp->pmt_retain += 1;
-					sprintf(chosen_sid, "%s %d", *chosen_sid ? chosen_sid : "", service_id);
+					//sprintf(chosen_sid, "%s %d", *chosen_sid ? chosen_sid : "", service_id);
+					strcatNum( chosen_sid, service_id);
 				}
 				else if(!strcasecmp(*p, "hd") || !strcasecmp(*p, "sd1")) {
 					/* hd/sd1 指定時には1番目のサービスを保存する */
@@ -482,7 +486,8 @@ static int AnalyzePat(splitter *sp, unsigned char *buf)
 						sid_found = TRUE;
 						sp->pmt_version[sp->pmt_retain].pid = pid;
 						sp->pmt_retain += 1;
-						sprintf(chosen_sid, "%s %d", *chosen_sid ? chosen_sid : "", service_id);
+						//sprintf(chosen_sid, "%s %d", *chosen_sid ? chosen_sid : "", service_id);
+						strcatNum( chosen_sid, service_id);
 					}
 				}
 				else if(!strcasecmp(*p, "sd2")) {
@@ -495,7 +500,8 @@ static int AnalyzePat(splitter *sp, unsigned char *buf)
 						sid_found = TRUE;
 						sp->pmt_version[sp->pmt_retain].pid = pid;
 						sp->pmt_retain += 1;
-						sprintf(chosen_sid, "%s %d", *chosen_sid ? chosen_sid : "", service_id);
+						//sprintf(chosen_sid, "%s %d", *chosen_sid ? chosen_sid : "", service_id);
+						strcatNum( chosen_sid, service_id);
 					}
 				}
 				else if(!strcasecmp(*p, "sd3")) {
@@ -508,7 +514,8 @@ static int AnalyzePat(splitter *sp, unsigned char *buf)
 						sid_found = TRUE;
 						sp->pmt_version[sp->pmt_retain].pid = pid;
 						sp->pmt_retain += 1;
-						sprintf(chosen_sid, "%s %d", *chosen_sid ? chosen_sid : "", service_id);
+						//sprintf(chosen_sid, "%s %d", *chosen_sid ? chosen_sid : "", service_id);
+						strcatNum( chosen_sid, service_id);
 					}
 				}
 				else if(!strcasecmp(*p, "1seg")) {
@@ -521,7 +528,8 @@ static int AnalyzePat(splitter *sp, unsigned char *buf)
 						sid_found = TRUE;
 						sp->pmt_version[sp->pmt_retain].pid = pid;
 						sp->pmt_retain += 1;
-						sprintf(chosen_sid, "%s %d", *chosen_sid ? chosen_sid : "", service_id);
+						//sprintf(chosen_sid, "%s %d", *chosen_sid ? chosen_sid : "", service_id);
+						strcatNum( chosen_sid, service_id);
 					}
 				}
 				else if(!strcasecmp(*p, "all")) {
@@ -533,7 +541,9 @@ static int AnalyzePat(splitter *sp, unsigned char *buf)
 					sid_found = TRUE;
 					sp->pmt_version[sp->pmt_retain].pid = pid;
 					sp->pmt_retain += 1;
-					sprintf(chosen_sid, "%s %d", *chosen_sid ? chosen_sid : "", service_id);
+					//sprintf(chosen_sid, "%s %d", *chosen_sid ? chosen_sid : "", service_id);
+					strcatNum( chosen_sid, service_id);
+
 					break;
 				}
 				else if(!strcasecmp(*p, "epg")) {
@@ -581,7 +591,8 @@ static int AnalyzePat(splitter *sp, unsigned char *buf)
 				sid_found = TRUE;
 				sp->pmt_version[sp->pmt_retain].pid = pid;
 				sp->pmt_retain += 1;
-				sprintf(chosen_sid, "%s %d", *chosen_sid ? chosen_sid : "", service_id);
+				//sprintf(chosen_sid, "%s %d", *chosen_sid ? chosen_sid : "", service_id);
+				strcatNum( chosen_sid, service_id);
 			}
 		}
 
@@ -852,4 +863,32 @@ static int GetPid(
 	unsigned char* data)				// [in]		取得対象データのポインタ
 {
 	return ((data[0] & 0x1F) << 8) + data[1];
+}
+
+/*
+ * epg抽出に必要なPIDのフラグを立てる
+ */
+static void addEpgPids( splitter *sp ) {
+  for( int i=0; sp->sid_list[i] != NULL; i++)
+	{
+	  if( ! strcasecmp( sp->sid_list[i], "epg")) {
+		//fprintf(stderr, "addEpgPids() %s\n",sp->sid_list[i]);
+		*(sp->pids+0x11) = 1;	// SDT
+		*(sp->pids+0x12) = 1;	// EIT
+		*(sp->pids+0x23) = 1;	// SDTT
+		*(sp->pids+0x29) = 1;	// CDT
+	  }
+	}
+}
+
+/* 文字列に数字を追加 */
+static char *strcatNum( char *chosen_sid, int service_id)
+{
+  char	tmp[512];
+  
+  if ( chosen_sid != NULL ) {
+	sprintf(tmp, " %d",service_id );
+	strcat( chosen_sid, tmp );
+  }
+  return chosen_sid ;
 }
